b92ad9e7717fbc515ae7465e086a1ec2281d9762
kevguy <kevlai22@uw.edu>
2016-09-21 10:25:53 +0800

    feat: will query api every 5 seconds

--- scripts/code.js
+++ scripts/code.js
@@ -1,16 +1,23 @@
-var quakes = Rx.DOM.jsonpRequest({
-		url: QUAKE_URL,
-		jsonpCallback: 'eqfeed_callback'
+var quakes = Rx.Observable
+	.interval(5000)
+	.flatMap(function(){
+		return Rx.DOM.jsonpRequest({
+				url: QUAKE_URL,
+				jsonpCallback: 'eqfeed_callback'
+			}).retry(3);
 	})
 	.flatMap(function(result){
 		return Rx.Observable.from(result.response.features);
+	})
+	.distinct(function(quake) {
+		return quake.properties.code;
 	})
 	.map(function(quake){
 		return {
 			lat: quake.geometry.coordinates[1],
 			lng: quake.geometry.coordinates[0],
-				size: quake.properties.mag * 10000
-		}
+			size: quake.properties.mag * 10000
+		};
 	});
 
 quakes.subscribe(function(quake){
